4face7c6bc2b5d633af087dcb9d7aaaed502c3f0
dwsindorf <dwsindorf@gmail.com>
2016-10-05 20:12:47 -0600

    fixes multiple merge (and other) problems with parallel checkin attempts from 2 teams

--- DiscusBot/src/Robot.cpp
+++ DiscusBot/src/Robot.cpp
@@ -1,8 +1,10 @@
 #include "WPILib.h"
-#define LIFTER_UP_SPEED 0.2
-#define LIFTER_DOWN_SPEED -0.2
+#define LIFTER_UP_SPEED 0.5
+#define LIFTER_DOWN_SPEED -0.5
 #define LIFTER_UP_BUTTON 5
 #define LIFTER_DOWN_BUTTON 6
+#define FLYWHEEL_BUTTON 2
+#define SHOOTER_BUTTON 1
 
 class Robot: public IterativeRobot {
 private:
@@ -12,7 +14,13 @@ private:
 	CANTalon *rearLeft;
 	CANTalon *rearRight;
 	CANTalon *liftMotor;
+	CANTalon *shooterMotor;
+	DoubleSolenoid *shooterPneumatic;
+
 	RobotDrive *robotDrive;
+	bool *wheelOutput;
+	bool *wheelPInput;
+
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
 	const std::string autoNameDefault = "Default";
@@ -21,11 +29,14 @@ private:
 
 	void RobotInit() {
 		stick = new Joystick(0);
+		shooterPneumatic = new DoubleSolenoid(7,0,1);
 		frontLeft = new CANTalon(1);
 		frontRight = new CANTalon(4);
 		rearLeft = new CANTalon(2);
 		rearRight = new CANTalon(3);
 		liftMotor = new CANTalon(5);
+		shooterMotor = new CANTalon(6);
+
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*) &autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*) &autoNameCustom);
@@ -33,64 +44,90 @@ private:
 		robotDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
 		robotDrive->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
 		robotDrive->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+		wheelOutput = new bool;
+		wheelPInput = new bool;
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	void AutonomousInit() {
-		autoSelected = *((std::string*) chooser->GetSelected());
-		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
-
-		if (autoSelected == autoNameCustom) {
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
 	}
 
 	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
 	}
 
 	void TeleopInit() {
-
+		*wheelPInput = false;
+		*wheelOutput = false;
 	}
 
 	void TeleopPeriodic() {
 		DriveWithJoystick();
 		DoLifter();
+		DoShooter ();
 	}
 
 	void TestPeriodic() {
 		lw->Run();
 	}
 	void DriveWithJoystick() {
-		robotDrive->MecanumDrive_Cartesian(stick->GetX(), stick->GetY(),
-				stick->GetZ());
+		// Get axis values
+		float yAxis = stick-> GetY();
+		float xAxis = stick-> GetX();
+		float zAxis = stick-> GetZ();
+		// Run axis values through deadband
+		yAxis = quadDeadband(.6, .3, yAxis);
+		xAxis = quadDeadband(.6, .3, xAxis);
+		zAxis = quadDeadband(.6, .3, zAxis);
+		robotDrive->MecanumDrive_Cartesian(xAxis, yAxis, zAxis);
+	}
+	void DoShooter ()
+	{
+		if (toggle(stick->GetRawButton(FLYWHEEL_BUTTON), wheelOutput, wheelPInput))
+			shooterMotor->Set(-1);
+		else
+			shooterMotor->Set(0);
+
+		if (stick->GetRawButton(SHOOTER_BUTTON))
+			shooterPneumatic->Set(DoubleSolenoid::kReverse);
+		else
+			shooterPneumatic->Set(DoubleSolenoid::kForward);
 	}
 	void DoLifter() {
 		if (stick->GetRawButton(LIFTER_UP_BUTTON)) {
 			liftMotor->Set(LIFTER_UP_SPEED);
-			printf("Lifter up button pressed\n");
 		}
-
 		else if (stick->GetRawButton(LIFTER_DOWN_BUTTON)) {
 			liftMotor->Set(LIFTER_DOWN_SPEED);
-			printf("Lifter down button pressed\n");
 		} else {
 			liftMotor->Set(0);
+		}
+	}
+
+	bool toggle(bool input, bool* loutput, bool* pinput) {
+		if (input && !(*pinput)) {
+			(*loutput) = !(*loutput);
+			(*pinput) = input;
+			return (*loutput);
+		} else {
+			(*pinput) = input;
+			return (*loutput);
+		}
+	}
+	float quadDeadband(float minThreshold, float minOutput, float input) {
+		if (input > minThreshold) {
+			return ((((1 - minOutput)
+					/ ((1 - minThreshold) * (1 - minThreshold)))
+					* ((input - minThreshold) * (input - minThreshold)))
+					+ minOutput);
+		} else {
+			if (input < (-1 * minThreshold)) {
+				return (((minOutput - 1)
+						/ ((minThreshold - 1) * (minThreshold - 1)))
+						* ((minThreshold + input) * (minThreshold + input)))
+						- minOutput;
+			}
+			else {
+				return 0;
+			}
 		}
 	}
 };
