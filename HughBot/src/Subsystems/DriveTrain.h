0722ef51cc66ed1095660b4a82ee70d122006149
Connor Thompson <darkkn1ght@skybeam.com>
2017-01-09 15:00:50 -0700

    First pass at 2017 Robot.

---
+++ HughBot/src/Subsystems/DriveTrain.h
@@ -0,0 +1,24 @@
+#ifndef DriveTrain_H
+#define DriveTrain_H
+#include "WPILib.h"
+#include "Commands/Subsystem.h"
+#include "CANTalon.h"
+
+class DriveTrain: public frc::Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	CANTalon frontLeft;
+	CANTalon frontRight;
+	CANTalon backLeft;
+	CANTalon backRight;
+	RobotDrive *drive;
+public:
+	DriveTrain();
+	void InitDefaultCommand();
+	float quadDeadband(float minThreshold, float minOutput, float input);
+	void Drive(float xAxis, float yAxis, float zAxis);
+};
+
+#endif
