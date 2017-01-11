bc29e814c9747c8f7e837b8bb15a102673069b41
Janessa Chenot <50448@psdschools.org>
2017-01-11 16:34:55 -0700

    Changes to Default command

--- HughBot/src/CommandBase.cpp
+++ HughBot/src/CommandBase.cpp
@@ -7,7 +7,7 @@
 // line should be repeated for each subsystem in the project.
 std::shared_ptr<DriveTrain> CommandBase::driveTrain;
 std::shared_ptr<GearSubsystem> CommandBase::gearSubsystem;
-
+shared_ptr<Vision> CommandBase::visionSubsystem;
 
 std::unique_ptr<OI> CommandBase::oi = std::make_unique<OI>();
 
@@ -21,5 +21,6 @@ void CommandBase::init()
 	// line should be repeated for each subsystem in the project.
 	driveTrain.reset(new DriveTrain());
 	gearSubsystem.reset(new GearSubsystem());
+	visionSubsystem.reset(new Vision());
 	oi.reset(new OI());
 }
